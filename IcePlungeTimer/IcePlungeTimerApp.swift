5c4e7baf4b13821dc227a673e71311eb64baf7ac
Daniel Lawless <danlawless1999@gmail.com>
2023-04-23 13:33:17 +1000

    Initial Commit

---
+++ IcePlungeTimer/IcePlungeTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  IcePlungeTimerApp.swift
+//  IcePlungeTimer
+//
+//  Created by Daniel Lawless on 23/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct IcePlungeTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IcePlungeTimerUITests/IcePlungeTimerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  IcePlungeTimerUITestsLaunchTests.swift
+//  IcePlungeTimerUITests
+//
+//  Created by Daniel Lawless on 23/4/2023.
+//
+
+import XCTest
+
+final class IcePlungeTimerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
